0896379d2747d47098d9edb92d77e3f8acc94b9d
fox <fox@fox-VirtualBox>
2019-09-06 13:20:24 -0500

    Punto 2 hecho?

--- Punto2/gremlins-webdriver/test/specs/gremlins-test.js
+++ Punto2/gremlins-webdriver/test/specs/gremlins-test.js
@@ -1,48 +1,56 @@
 function loadScript(callback) {
-  var s = document.createElement('script');
-  s.src = 'https://rawgithub.com/marmelab/gremlins.js/master/gremlins.min.js';
-  if (s.addEventListener) {
-    s.addEventListener('load', callback, false);
-  } else if (s.readyState) {
-    s.onreadystatechange = callback;
-  }
-  document.body.appendChild(s);
+    var s = document.createElement('script');
+    s.src = 'https://rawgithub.com/marmelab/gremlins.js/master/gremlins.min.js';
+    if (s.addEventListener) {
+        s.addEventListener('load', callback, false);
+    } else if (s.readyState) {
+        s.onreadystatechange = callback;
+    }
+    document.body.appendChild(s);
 }
 
 function unleashGremlins(ttl, callback) {
-  function stop() {
-    horde.stop();
-    callback();
-  }
-  var notDisabledFormGremblin =gremlins.species.formFiller().canFillElement(function(element) {
-      return !element.hasAttribute("disabled");
-  });
-  var horde = window.gremlins.createHorde().gremlin(notDisabledFormGremblin);
-  horde.seed(1234);
-
-  horde.after(callback);
-  window.onbeforeunload = stop;
-  setTimeout(stop, ttl);
-  horde.unleash();
+    function stop() {
+        horde.stop();
+        callback();
+    }
+    var notDisabledFormGremblin = gremlins.species.formFiller().canFillElement(function (element) {
+        return !element.hasAttribute("disabled");
+    });
+    var clicksOnButtonsLinksGremblin = gremlins.species.clicker().clickTypes(['click']).canClick(function (element) {
+        if (element.tagName === "A" || element.tagName === "BUTTON") {
+            return true;
+        }
+        return false;
+    });
+    var horde = window.gremlins.createHorde().gremlin(notDisabledFormGremblin).gremlin(clicksOnButtonsLinksGremblin);
+    horde.seed(1234);
+horde.strategy(gremlins.strategies.distribution()
+  .distribution([0.3, 0.7]) // the first three gremlins have more chances to be executed than the last
+);
+    horde.after(callback);
+    window.onbeforeunload = stop;
+    setTimeout(stop, ttl);
+    horde.unleash();
 }
 
-describe('Monkey testing with gremlins ', function() {
+describe('Monkey testing with gremlins ', function () {
 
-  it('it should not raise any error', function() {
-    browser.url('/');
-    browser.click('button=Cerrar');
+    it('it should not raise any error', function () {
+        browser.url('/');
+        browser.click('button=Cerrar');
 
-    browser.timeoutsAsyncScript(60000);
-    browser.executeAsync(loadScript);
+        browser.timeoutsAsyncScript(60000);
+        browser.executeAsync(loadScript);
 
-    browser.timeoutsAsyncScript(60000);
-    browser.executeAsync(unleashGremlins, 50000);
-  });
-
-  afterAll(function() {
-    browser.log('browser').value.forEach(function(log) {
-      browser.logger.info(log.message.split(' ')[2]);
+        browser.timeoutsAsyncScript(60000);
+        browser.executeAsync(unleashGremlins, 50000);
     });
-  });
+
+    afterAll(function () {
+        browser.log('browser').value.forEach(function (log) {
+            browser.logger.info(log.message.split(' ')[2]);
+        });
+    });
 
 });
